2df9fe6f167d6e2ca6cd116bcda1253465da5000
Thiago Xavier <65921775+ThiagoXavierCP@users.noreply.github.com>
2023-07-09 23:14:33 -0300

    Add files via upload

---
+++ Aula_5.py
@@ -0,0 +1,12 @@
+#Variáveis são usadas para salvar algo na memoria do pc
+#Iniciar as variaveis com letras minusculas, pode usar numeros e underline
+#O sinal de = é o operador de atribuição. Atribui valor a um nome 
+nome_completo = 'Thiago Xavier' #Declarando valores
+soma_dois_mais_dois = 2 + 2 #Declarando valores
+print(nome_completo, soma_dois_mais_dois) #Printando valores
+
+nome = 'Thiago' #Declarando valores
+idade = 21 #Declarando valores
+maior_idade = idade >= 18 #Declarando valores
+print('Nome:', nome,  'Idade:', idade)
+print('É maior?:', maior_idade)
