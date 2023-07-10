df0960d57d0c1ec35a23d651f6325af44dc358ab
Thiago Xavier <65921775+ThiagoXavierCP@users.noreply.github.com>
2023-07-09 23:13:06 -0300

    Add files via upload

---
+++ Aula_7.py
@@ -0,0 +1,23 @@
+adicao = 10 + 10
+print('Adicção:', adicao)
+
+subtracao = 10 - 5
+print('Subtração:', subtracao)
+
+multiplicacao = 2 * 2
+print('Multiplicação', multiplicacao)
+
+divisao = 5 / 5 #-> Retorna float
+print('Divisão:', divisao)
+
+divisao_inteira = 5 // 5 #-> Retorna inteiro ou arredonda valor
+print('Divisão inteira:', divisao_inteira)
+
+exponeciacao = 2 ** 10 #-> Numero multiplicado por ele mesmo 10 vzs
+print('Exponencição:', exponeciacao)
+
+modulo = 25 % 5 #-> Resto da divisão
+print('Modulo:', modulo)
+
+
+
