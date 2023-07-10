6f5a4b69a01a2cb201cb397c01b4070863a3f324
Thiago Xavier <65921775+ThiagoXavierCP@users.noreply.github.com>
2023-07-09 23:12:11 -0300

    Add files via upload

---
+++ Aula_2.py
@@ -0,0 +1,16 @@
+# Metodo para printar numeros 
+print(1234)
+
+#Aspas simples já identifica que é uma string
+print('Thiago Xavier')
+
+#Aspas duplas
+print("Thiago Xavier")
+
+#Escape - Serve para pular o que foi digitado no codigo
+print("Thiago \"Xavier\"")
+
+#r - Serve para expressões regulares ou para exibição do escape
+print(r"Thiago \"Xavier\"")
+
+
