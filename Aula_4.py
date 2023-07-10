4b172a385352d6e75230cbdfeb24ffe297823839
Thiago Xavier <65921775+ThiagoXavierCP@users.noreply.github.com>
2023-07-09 23:14:19 -0300

    Add files via upload

---
+++ Aula_4.py
@@ -0,0 +1,10 @@
+#Conversão de tipos, coerção
+#type convertion, typecasting, coercion
+#é o ato de converter um numero em outro
+#tipos imutaveis e primitos:
+#str, int, float, bool
+print(int ('1'), type(int('1'))) #-> Printa o numero por ter int e o seu tipo
+print(int('1') + 1) #->Faz a soma dos numeros por entender como int e não float
+print(float('2') + 2) #->Faz a soma dos numeros por entender como float
+print(bool('')) #-> String vazia sem espaço considera como false 
+print(str(11) + 'b') #->Concatena os valores
