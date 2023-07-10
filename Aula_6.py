407ddf6f3a83170ca7c76cdba086b1da21536510
Thiago Xavier <65921775+ThiagoXavierCP@users.noreply.github.com>
2023-07-09 23:14:50 -0300

    Add files via upload

---
+++ Aula_6.py
@@ -0,0 +1,15 @@
+#Exercicio: Declarando valor de variaveis
+
+nome = 'Thiago'
+sobrenome = 'Xavier'
+idade = 21
+ano_nascimento = 2023 - idade
+maior_de_idade = idade >=18
+altura_metros = 1.79
+
+print('Nome:', nome)
+print('Sobrenome:',sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade?:', maior_de_idade)
+print('Qual sua altura?:', altura_metros)
