bae46157f1dce406dfea36a3edfbaeb1ac3a3d13
Thiago Xavier <65921775+ThiagoXavierCP@users.noreply.github.com>
2023-07-09 23:11:37 -0300

    Add files via upload

---
+++ Aula_1.py
@@ -0,0 +1,7 @@
+# - Permite comentar o codigo 
+
+#Comando sep serve para escolher o separador do numero:
+#Comando end serve geralmente para realizar quebra de linhas:
+
+print(12, 34, sep="-", end='\n') 
+print(56, 78, sep= '-', end='\n') 
